b508c06871a71b58ce8e003b6e55f0f3e812252f
danetomseth <danetomseth@gmail.com>
2016-06-11 19:30:26 -0700

    video layout

--- blank-fsg/browser/js/grid/grid.js
+++ blank-fsg/browser/js/grid/grid.js
@@ -2,11 +2,21 @@ app.config(function($stateProvider) {
     $stateProvider.state('grid', {
         url: '/grid',
         controller: 'GridCtrl',
-        templateUrl: 'js/grid/grid.html'
+        templateUrl: 'js/grid/grid.html',
+        resolve: {
+            videos: function(GridFactory) {
+                return GridFactory.fetchVideos( ).then(function(res) {
+                    return res.data.data;
+                });
+            }
+        }
     });
 });
 
-app.controller('GridCtrl', function($scope, GridFactory) {
+app.controller('GridCtrl', function($scope, GridFactory, videos) {
+    $scope.videoPlaying = true;
+    $scope.videos = videos;
+    console.log("videos", videos);
     $scope.text = 'Hello';
     $scope.items = [];
     $scope.videoLoad = false;
@@ -36,51 +46,51 @@ app.controller('GridCtrl', function($scope, GridFactory) {
 
 app.directive('videoElem', function($rootScope, AuthService, AUTH_EVENTS, $state, $sce, $uibModal) {
 
-        return {
-            restrict: 'E',
-            scope: {
-                video: '=',
-                width: '='
-            },
-            controller: "ModalCtrl",
-            templateUrl: 'js/grid/video.html',
-            link: function(scope, elem, attr) {
+    return {
+        restrict: 'E',
+        scope: {
+            video: '=',
+            width: '='
+        },
+        controller: "ModalCtrl",
+        templateUrl: 'js/grid/video.html',
+        link: function(scope, elem, attr) {
 
-                let src = "https://player.vimeo.com/video/";
-                var regexp = /\d+/gi;
-                scope.picture = scope.video.pictures.sizes[2].link;
-                var videoId = scope.video.uri.match(regexp);
-                scope.vidLink = src + videoId + '?badge=0&autopause=0&player_id=0';
+            let src = "https://player.vimeo.com/video/";
+            var regexp = /\d+/gi;
+            scope.picture = scope.video.pictures.sizes[2].link;
+            var videoId = scope.video.uri.match(regexp);
+            scope.vidLink = src + videoId + '?badge=0&autopause=0&player_id=0';
 
-                scope.trustSrc = function(src) {
-                    return $sce.trustAsResourceUrl(src);
-                }
-
-                // scope.height = scope.width * .5625;
-                var box = document.getElementById("wrapper");
-                console.log('height', scope.height);
-                scope.openModal = function() {
-
-                    var modalInstance = $uibModal.open({
-                        animation: true,
-                        appendTo: box,
-                        scope: scope,
-                        templateUrl: 'js/grid/video-window.html',
-                        controller: 'ModalCtrl',
-                        size: 'Lg'
-                    });
-
-                }
+            scope.trustSrc = function(src) {
+                return $sce.trustAsResourceUrl(src);
             }
-        };
+
+            // scope.height = scope.width * .5625;
+            var box = document.getElementById("wrapper");
+            console.log('height', scope.height);
+            scope.openModal = function() {
+
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    appendTo: box,
+                    scope: scope,
+                    templateUrl: 'js/grid/video-window.html',
+                    controller: 'ModalCtrl',
+                    size: 'Lg'
+                });
+
+            }
+        }
+    };
 });
 
 
-    function Modal($scope, $uibModal) {
+function Modal($scope, $uibModal) {
 
-    }
-
-
+}
 
 
-    app.controller('ModalCtrl', Modal);
+
+
+app.controller('ModalCtrl', Modal);
